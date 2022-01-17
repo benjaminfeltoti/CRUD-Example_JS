1a4d2e0d99c4ca4544c16a8e3c1a6823e957c427
Feltoti <benjamin.feltoti@zeiss.com>
2022-01-17 06:42:51 +0100

    Add project files.

---
+++ WebApplication2/Controllers/WeatherController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        // GET: api/<WeatherController>
+        [HttpGet]
+        public IEnumerable<WeatherForecastModel> Get()
+        {
+            var rng = new Random();
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecastModel
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray();
+        }
+
+        // GET api/<WeatherController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<WeatherController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<WeatherController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<WeatherController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApplication2/wwwroot/js/site.js
@@ -0,0 +1,48 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function load() {
+
+    console.log("DOM Loaded.");
+
+    getApi();
+
+    async function getApi(url = "https://localhost:44365/api/weather") {
+        // Get Data
+        const response = await fetch(url);
+        let data = await response.json();
+        
+        // Load into table
+        showTable(data);
+    }
+    
+    function showTable(data){
+        let tableContent = 
+        `<tr>
+            <th>Time</th>
+            <th>Weather</th>
+            <th>Celsius</th>
+            <th>Fahrenheit</th>`;
+
+        for (const item of data) {
+            tableContent += `
+            <tr>
+                <td>${item.date}</td>
+                <td>${item.summary}</td>
+                <td>${item.temperatureC} C°</td>
+                <td>${item.temperatureF} F°</td>
+            </tr>`;            
+        }
+
+        document.getElementById("contentTable").innerHTML=tableContent;
+    }
+
+
+}
+if (document.readyState == "loading") {
+    document.addEventListener("DOMContentLoaded", load);
+}
+else {
+    load();
+}
