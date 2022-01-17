f41fb6a16153eecaa7b3cf455e13ed1da4119baf
Benjamin Feltoti <benjamin.feltoti@gmail.com>
2022-01-17 12:57:36 +0100

    Implement PUT

--- WebApplication2/wwwroot/js/site.js
+++ WebApplication2/wwwroot/js/site.js
@@ -1,7 +1,7 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-var GetWeatherForecasts, CreateNewWeatherForecast;
+var GetWeatherForecasts, CreateNewWeatherForecast, UpdateWeatherForecast;
 
 var app = function () {
 
@@ -14,16 +14,18 @@ var app = function () {
         const response = await fetch(url);
         let data = await response.json();
 
-        showTable(data);
+        processDataForElements(data);
     }
 
-    function showTable(data) {
+    function processDataForElements(data) {
         let tableContent =
             `<tr>
             <th>Time</th>
             <th>Weather</th>
             <th>Celsius</th>
             <th>Fahrenheit</th>`;
+
+        let comboboxContent;
 
         for (const item of data) {
             tableContent += `
@@ -33,19 +35,24 @@ var app = function () {
                 <td>${item.temperatureC} C°</td>
                 <td>${item.temperatureF} F°</td>
             </tr>`;
+
+            comboboxContent += `
+            <option>${item.id}</option>
+            `;
         }
 
         document.getElementById("contentTable").innerHTML = tableContent;
+        document.getElementById("changeIdCombobox").innerHTML = comboboxContent;
     }
 
-    async function CreateNewWeatherForecastEntity() {
+    async function createNewWeatherForecast() {
         console.log("POSTING WAS CALLED!");
 
         const date = document.getElementById("dateInput").value;
         const temperatureC = Number(document.getElementById("celsiusInput").value);
         const summary = document.getElementById("summaryInput").value;
 
-        let weatherForecastObject = { id: 99, date: date, temperatureC: temperatureC, summary: summary };
+        let weatherForecastObject = { id: -1, date: date, temperatureC: temperatureC, summary: summary };
         let jsonstring = JSON.stringify(weatherForecastObject);
         console.log(jsonstring);
         let response = await fetch("api/weather", {
@@ -61,8 +68,33 @@ var app = function () {
         console.log(content); */
     }
 
+    async function updateWeatherForecast() {
+        console.log("PUT WAS CALLED!");
+
+        const id = Number(document.getElementById("changeIdCombobox").value);
+        const date = document.getElementById("dateInputChange").value;
+        const temperatureC = Number(document.getElementById("celsiusInputChange").value);
+        const summary = document.getElementById("summaryInputChange").value;
+
+        let weatherForecastObject = { id: id, date: date, temperatureC: temperatureC, summary: summary };
+        let jsonstring = JSON.stringify(weatherForecastObject);
+        console.log(jsonstring);
+        let response = await fetch(`api/weather/${id}`, {
+            method: "PUT",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: jsonstring
+        });
+        // TODO : Handle bad request
+        /* const content = await response.json();
+        console.log(content); */
+    }
+
     GetWeatherForecasts = getWeatherForecasts;
-    CreateNewWeatherForecast = CreateNewWeatherForecastEntity;
+    CreateNewWeatherForecast = createNewWeatherForecast;
+    UpdateWeatherForecast = updateWeatherForecast;
 }
 if (document.readyState == "loading") {
     document.addEventListener("DOMContentLoaded", app);
