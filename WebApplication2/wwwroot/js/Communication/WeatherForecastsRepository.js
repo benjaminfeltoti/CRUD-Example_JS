296b9ddceaa08444442bf8e0b7357db78bda3f46
Benjamin Feltoti <benjamin.feltoti@gmail.com>
2022-01-18 21:27:29 +0100

    Move api calls to repo

--- WebApplication2/wwwroot/js/Communication/WeatherForecastsRepository.js
+++ WebApplication2/wwwroot/js/Communication/WeatherForecastsRepository.js
@@ -4,4 +4,71 @@ async function getWeatherForecasts() {
     return await response.json();
 }
 
-export default getWeatherForecasts;
+async function createNewWeatherForecast() {
+    console.log("POSTING WAS CALLED!");
+
+    const date = document.getElementById("dateInput").value;
+    const temperatureC = Number(document.getElementById("celsiusInput").value);
+    const summary = document.getElementById("summaryInput").value;
+
+    let weatherForecastObject = { id: -1, date: date, temperatureC: temperatureC, summary: summary };
+    let jsonstring = JSON.stringify(weatherForecastObject);
+    console.log(jsonstring);
+    let response = await fetch("api/weather", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: jsonstring
+    });
+    // TODO : Handle bad request
+    /* const content = await response.json();
+    console.log(content); */
+}
+
+async function updateWeatherForecast() {
+    console.log("PUT WAS CALLED!");
+
+    const id = Number(document.getElementById("changeIdCombobox").value);
+    const date = document.getElementById("dateInputChange").value;
+    const temperatureC = Number(document.getElementById("celsiusInputChange").value);
+    const summary = document.getElementById("summaryInputChange").value;
+
+    let weatherForecastObject = { id: id, date: date, temperatureC: temperatureC, summary: summary };
+    let jsonstring = JSON.stringify(weatherForecastObject);
+    console.log(jsonstring);
+    let response = await fetch(`api/weather/${id}`, {
+        method: "PUT",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: jsonstring
+    });
+    // TODO : Handle bad request
+    /* const content = await response.json();
+    console.log(content); */
+}
+
+async function deleteWeatherForecast() {
+    console.log("DELETE WAS CALLED!");
+
+    const id = Number(document.getElementById("deleteIdCombobox").value);
+    let response = await fetch(`api/weather/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: ""
+    });
+    // TODO : Handle bad request
+    /* const content = await response.json();
+    console.log(content); */
+
+    //IFFIE
+    //(function(){//code}())
+}
+
+export {getWeatherForecasts, createNewWeatherForecast, updateWeatherForecast, deleteWeatherForecast};
--- WebApplication2/wwwroot/js/site.js
+++ WebApplication2/wwwroot/js/site.js
@@ -1,7 +1,7 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-import getWeatherForecasts from "./Communication/WeatherForecastsRepository.js";
+import {getWeatherForecasts, createNewWeatherForecast, updateWeatherForecast, deleteWeatherForecast} from "./Communication/WeatherForecastsRepository.js";
 
 var app = async function () {
 
@@ -17,6 +17,8 @@ var app = async function () {
         document.getElementById("postSubmitButton").addEventListener("click", createNewWeatherForecast);
         document.getElementById("updateSubmitButton").addEventListener("click", updateWeatherForecast);
         document.getElementById("deleteSubmitButton").addEventListener("click", deleteWeatherForecast);
+        
+        // Refresh content of comboboxes on opening
     }
 
     async function processDataForElements(data) {
@@ -47,73 +49,6 @@ var app = async function () {
         document.getElementById("contentTable").innerHTML = tableContent;
         document.getElementById("changeIdCombobox").innerHTML = comboboxContent;
         document.getElementById("deleteIdCombobox").innerHTML = comboboxContent;
-    }
-
-    async function createNewWeatherForecast() {
-        console.log("POSTING WAS CALLED!");
-
-        const date = document.getElementById("dateInput").value;
-        const temperatureC = Number(document.getElementById("celsiusInput").value);
-        const summary = document.getElementById("summaryInput").value;
-
-        let weatherForecastObject = { id: -1, date: date, temperatureC: temperatureC, summary: summary };
-        let jsonstring = JSON.stringify(weatherForecastObject);
-        console.log(jsonstring);
-        let response = await fetch("api/weather", {
-            method: "POST",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            body: jsonstring
-        });
-        // TODO : Handle bad request
-        /* const content = await response.json();
-        console.log(content); */
-    }
-
-    async function updateWeatherForecast() {
-        console.log("PUT WAS CALLED!");
-
-        const id = Number(document.getElementById("changeIdCombobox").value);
-        const date = document.getElementById("dateInputChange").value;
-        const temperatureC = Number(document.getElementById("celsiusInputChange").value);
-        const summary = document.getElementById("summaryInputChange").value;
-
-        let weatherForecastObject = { id: id, date: date, temperatureC: temperatureC, summary: summary };
-        let jsonstring = JSON.stringify(weatherForecastObject);
-        console.log(jsonstring);
-        let response = await fetch(`api/weather/${id}`, {
-            method: "PUT",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            body: jsonstring
-        });
-        // TODO : Handle bad request
-        /* const content = await response.json();
-        console.log(content); */
-    }
-
-    async function deleteWeatherForecast() {
-        console.log("DELETE WAS CALLED!");
-
-        const id = Number(document.getElementById("deleteIdCombobox").value);
-        let response = await fetch(`api/weather/${id}`, {
-            method: "DELETE",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            body: ""
-        });
-        // TODO : Handle bad request
-        /* const content = await response.json();
-        console.log(content); */
-
-        //IFFIE
-        //(function(){//code}())
     }
 }
 if (document.readyState == "loading") {
